60247c13d4fcac902a066df50b140024d87781f5
Awaiz14 <100952563+Awaiz14@users.noreply.github.com>
2023-10-10 00:14:29 +0100

    Add files via upload

--- HTML/SignUp.js
+++ HTML/SignUp.js
@@ -1,27 +1,44 @@
 function validateForm() {
     var firstName = document.getElementById("first-name").value;
-    var Surname = document.getElementById("surname").value;
+    var surname = document.getElementById("surname").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     
     if (firstName === "") {
     alert("Please enter your First Name.");
-    return false;
+    }  else if (/\d/.test(firstName)) {
+        alert("First name cannot contain numbers");
+    } 
+    else {
+        localStorage.setItem("firstName",
+        firstName);
     }
 
-    if (Surname === "") {
+    if (surname === "") {
     alert("Please enter your Last Name.");
-    return false;
+    } else {
+        localStorage.setItem("surname",
+        surname);
     }
 
     if (email === "") {
     alert("Please enter your Email.");
-    return false;
+    } else if (!email.includes("@")) {
+    alert ("Enter a valid email with @")
+    }
+    else {
+        localStorage.setItem("email",
+        email);
     }
 
     if (password === "") {
     alert("Please enter a Password");
-    return false;
+    } else if (password.length < 6) {
+        alert("Password should be atleast 6 characters")
+    }
+    else {
+        localStorage.setItem("password",
+        password);
     }
     
     // Additional validation logic for other fields
