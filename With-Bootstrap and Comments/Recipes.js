d42f30f511ade26abd455751341de5662f03121d
Awaiz14 <100952563+Awaiz14@users.noreply.github.com>
2023-10-25 13:32:54 +0100

    Add files via upload
    Upload with comments added to every file and CSS validation logo on each page

--- With-Bootstrap and Comments/Recipes.js
+++ With-Bootstrap and Comments/Recipes.js
@@ -1,21 +1,24 @@
-function searchCards() {
-    const searchInput = document.getElementById('searchInput');
-    const searchTerm = searchInput.value.toLowerCase().replace(/\s/g, '');
-    const cardTitles = document.querySelectorAll('.card-title');
-    let hasResults = false;
+// MY OWN CODE
+function searchCards() { // Creates a function 
+    const searchInput = document.getElementById('searchInput'); //Gets the input value
+    const searchTerm = searchInput.value.toLowerCase().replace(/\s/g, ''); //Converts the input to lower case
+    const cardTitles = document.querySelectorAll('.card-title'); //Method allowing to select multiple elements
+    let hasResults = false; //Keeps track of results
    
-    cardTitles.forEach(function(title) {
-      const cardTitle = title.textContent.toLowerCase().replace(/\s/g, '');
-      const card = title.closest('.card');
+    cardTitles.forEach(function(title) { //For each goes through each element and performs a function on it that takes argument title
+      const cardTitle = title.textContent.toLowerCase().replace(/\s/g, ''); //Takes card title and converts to lowercase
+      const card = title.closest('.card'); //looks for closest element that has class card and storing in this variable
    
-      if (cardTitle.includes(searchTerm)) {
-        card.style.display = 'block';
-        hasResults = true;
+      if (cardTitle.includes(searchTerm)) { //if check for matching input with card title
+        card.style.display = 'block'; // displays recipe
+        hasResults = true; // Has a result if its true
       } else {
-        card.style.display = 'none';
+        card.style.display = 'none'; //Hides recipes
       }
     });
     if (!hasResults){
-        alert("No results")
+        alert("No results") //No results alert
     }
    }
+
+  //  END OF MY OWN CODE
--- With-Bootstrap and Comments/SignUp.js
+++ With-Bootstrap and Comments/SignUp.js
@@ -1,21 +1,23 @@
-function validateForm() {
-    var firstName = document.getElementById("first-name").value;
-    var surname = document.getElementById("surname").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+// MY OWN CODE
+function validateForm() { //Create function which validates form
+    var firstName = document.getElementById("first-name").value; //Gets first name input
+    var surname = document.getElementById("surname").value; //Gets surname input
+    var email = document.getElementById("email").value; //Gets email input
+    var password = document.getElementById("password").value; //Gets password input
     
-    if (firstName === "") {
-    alert("Please enter your First Name.");
+    if (firstName === "") { //IF statement to do checks
+    alert("Please enter your First Name."); //Alerts if first name is empty
     return false;
-    }  else if (/\d/.test(firstName)) {
-        alert("First name cannot contain numbers");
+    }  else if (/\d/.test(firstName)) { //Checks if numbers are included
+        alert("First name cannot contain numbers"); //Alerts cannot contain numbers if included
         return false;
     } 
     else {
-        localStorage.setItem("firstName",
+        localStorage.setItem("firstName", //Stores correct input of first name in local storage
         firstName);
     }
 
+    //Copy pasted above code for every other input box and added own validation criteria for each
     if (surname === "") {
     alert("Please enter your Last Name.");
     return false;
@@ -24,7 +26,8 @@ function validateForm() {
         surname);
     }
 
-    if (email === "") {
+    //Copy pasted above code for every other input box and added own validation criteria for each
+    if (email === "") { 
     alert("Please enter your Email.");
     return false;
     } else if (!email.includes("@")) {
@@ -36,6 +39,7 @@ function validateForm() {
         email);
     }
 
+    //Copy pasted above code for every other input box and added own validation criteria for each
     if (password === "") {
     alert("Please enter a Password");
     return false;
@@ -48,11 +52,13 @@ function validateForm() {
         password);
     }
     
-    // Additional validation logic for other fields
+    // Can add more validation checks if you have more input boxes
     
-    alert("You are now a member!");
-    window.location.href = "HomePage.html";
+    alert("You are now a member!"); //Alert when all details are entered succesfully with no errors
+    window.location.href = "HomePage.html"; //Takes user to homepage once signed up
     return true;
    
 }
+
+// END OF MY OWN CODE
 
